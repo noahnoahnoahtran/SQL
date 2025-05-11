a9fd62a507147920361d9c16fc8242135bbafae5
noahnoahnoahtran <tranthaingochuong@gmail.com>
2025-05-11 13:17:55 +0700

    Edit silver script

--- warehouse1/scripts/silver/proc_load_silver.sql
+++ warehouse1/scripts/silver/proc_load_silver.sql
@@ -6,7 +6,6 @@ INSERT INTO silver.crm_cust_info (
 	cst_marital_status,
 	cst_gndr,
 	cst_create_date)
-
 SELECT
 	cst_id,
 	cst_key,
@@ -29,3 +28,48 @@ FROM(
 	WHERE cst_id IS NOT NULL
 )t
 WHERE flag_last = 1; -- Select the most recent record per customer
+
+INSERT INTO silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key, -- Extract product key
+	prd_nm,
+	ISNULL(prd_cost, 0) AS prd_cost,
+	CASE UPPER(TRIM(prd_line))
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'n/a'
+	END AS prd_line, -- Map product line codes to descriptive values
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+	CAST(
+		LEAD(prd_start_dt) OVER (PARTITION BY prd_Key ORDER BY prd_start_dt) - 1 
+		AS DATE)
+	AS prd_end_dt -- Calculate end date as one day before the next start date
+FROM bronze.crm_prd_info
+
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END AS sls_order_dt,
+	CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END AS sls_ship_dt,
+	CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END AS sls_due_dt
+FROM bronze.crm_sales_details
