26dd53f1af360a1841643163e92b315e8b9b5870
noahnoahnoahtran <tranthaingochuong@gmail.com>
2025-05-22 10:02:57 +0700

    Update EDA_draft

--- sales_data_warehouse/EDA_draft.sql
+++ sales_data_warehouse/EDA_draft.sql
@@ -239,3 +239,27 @@ GROUP BY
 	c.first_name,
 	c.last_name
 ORDER BY total_orders
+
+-- ============================================
+-- Change Over Time Analysis
+-- ============================================
+
+SELECT
+	DATETRUNC(month, order_date) AS order_date,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(month, order_date)
+ORDER BY DATETRUNC(month, order_date)
+
+SELECT
+	FORMAT(order_date, 'yyyy-MMM') AS order_date,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS total_customers,
+	SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY FORMAT(order_date, 'yyyy-MMM')
+ORDER BY FORMAT(order_date, 'yyyy-MMM')
