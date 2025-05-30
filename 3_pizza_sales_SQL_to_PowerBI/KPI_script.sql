4b968c69f65265e8e67138f421a96f8d1235cc77
noahnoahnoahtran <tranthaingochuong@gmail.com>
2025-05-30 23:28:53 +0700

    Update script

--- 3_pizza_sales_SQL_to_PowerBI/KPI_script.sql
+++ 3_pizza_sales_SQL_to_PowerBI/KPI_script.sql
@@ -2,3 +2,25 @@
 
 SELECT SUM(total_price) AS total_revenue
 FROM dbo.pizza_sales
+
+-- Average order value
+
+SELECT SUM(total_price) / COUNT (DISTINCT order_id) AS average_order_value
+FROM dbo.pizza_sales
+
+-- Total # pizza sold
+
+SELECT SUM(quantity) AS total_pizza_sold
+FROM dbo.pizza_sales
+
+-- Total # order placed
+
+SELECT COUNT(DISTINCT order_id) AS total_order_placed
+FROM dbo.pizza_sales
+
+-- Average # pizza per order
+
+SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10, 2)) / CAST(COUNT(DISTINCT order_id) AS DECIMAL(10, 2)) AS DECIMAL(10, 2)) AS average_pizza_per_order
+FROM dbo.pizza_sales
+
+
